6ad18810d9680ec45808b7a02bebeab6ee27aece
megan88-collab <megan.evans88@outlook.com>
2024-08-05 16:29:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+
+#Exercise 10
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+print(i1+(i2*i3))
+print(i1*(i2+i3))
+print(i1/(i2+i3))
+print(i1//(i2+i3))
+print(i1/i2+i3)
+print(i1//i2+i3)
+print(3+4+5/3)
+print(3+4+5//3)
+print((3+4+5)/3)
+print((3+4+5)//3)
+print(d1+(d2*d3))
+print(d1+d2*d3)
+print(d1/d2-d3)
+print(d1/(d2-d3))
+print(d1+d2+d3/3)
+print((d1+d2+d3)/3)
+print(d1+d2+(d3/3))
+print(3*(d1+d2)*(d1-d3))
+
+#ApplicationsSlide26
+Age = 28
+Temperature = 98.6
+first_name = "Megan"
+last_name = "Evans"
+full_name = first_name + ' ' + last_name
+print(full_name)
+num1 = 15
+num2 = 20
+print(num1 + num2)
+print(num2 - num1)
+print(num1 * num2)
+print(num2 / num1)
+print(num2 // num1)
+print(num1**num2)
+print(num2%num1)
+
+#ApplicationsSlide29
+a = 38
+b = 6
+print(a+b) #Output - 44
+print(b-a) #Output - -32
+print(a*b) #Output - 228
+print(a/b) #Output - 6.333333333333333
+#Second question
+c = (a//b)
+d = (a**b)
+base = 10
+print(base**3)  #Output - 1000
+print(3**3)  #Output - 27
+x = 150
+y = 75
+z = 30
+print(x+y+z) #Output - 255
+print(x*y*z) #Output - 337500
+print(x/y/z) #Output - 0.06666666666666667
+print(z-(x+y)) #Output - -195
+print(x*(y-z))  #Output - 6750
+
+#Exercise 17 to 18
+print(3.14, int(3.14)) #Output - 3.14 3
+print(3.9999, int(3.9999)) #Output - 3.9999 3
+print(3.0, int(3.0)) #output - 3.0 3
+print(-3.9999, int(-3.9999)) #output - -3.999 -3
+print("2345", int("2345")) #output - 2345 2345
+print(17, int(17))  #output - 17 17
+
+val = 50+5
+print("the value is:" + str(val)) #output - the value is: 55
+
+
+#Second exercise page
+string = "3.14"
+num = float(string)
+print(num)
+print(type(num)) #output <class 'float'>
+num = "3.14"
+print(type(num))
+converted_num=
+
+
+string = "Python"
+print(string) #output - Python
+print()
